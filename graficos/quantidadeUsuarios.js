a5e972ec936e8f1e1e5efb11ed615dccc09fce3f
carloseduuejfj <110061942+carloseduuejfj@users.noreply.github.com>
2024-10-21 13:57:44 +0000

    .

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS } from "./common.js";
+
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -5,18 +7,25 @@ async function quantidadeUsuariosPorRede() {
     const nomeDasRedes = Object.keys(dados)
     const quantidadeDeUsuarios = Object.values(dados)
   
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeDeUsuarios,
-      type: 'bar'
-    }
-  ]
+    const data = [
+      {
+        x: 'nomeDasRedes',
+        y: quantidadeDeUsuarios,
+        type: 'bar',
+        marker: {
+          color: getCSS('--primary-color')
+        }
+      }
+    ]
   
+    const layout = {
+      plot_bgcolor: getCSS('--bg-color'),
+      paper_bgcolor: getCSS('--bg-color')
+  }
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+  Plotly.newPlot(grafico, data, layout)
   
   }
   
