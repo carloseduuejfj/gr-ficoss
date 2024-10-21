2e32e1a76103c7407aff93d6ec06d04a6f2e9a0e
carloseduuejfj <110061942+carloseduuejfj@users.noreply.github.com>
2024-10-21 14:10:59 +0000

    .

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -9,7 +9,7 @@ async function quantidadeUsuariosPorRede() {
   
     const data = [
       {
-        x: 'nomeDasRedes',
+        x: nomeDasRedes,
         y: quantidadeDeUsuarios,
         type: 'bar',
         marker: {
